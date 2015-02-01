d30bff178ba390d98ac953d21662e4042f6c2534
miql <mxander@gmail.com>
2015-01-31 18:23:59 -0700

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,24 @@ var gulp = require('gulp'),
 
 // general js code quality task that detects errors 
 // and potential problems
-gulp.task('jshint', function() {
-  // server-side js
-  return gulp.src([
+
+var serverJS = [
     'routes/**/*.js',
     'routes/**/*.js',
     'gulpfile.js',
-    'app.js'
-  ]).pipe($.jshint())
+    'app.js',
+    'models/**/*.js'
+];
+
+gulp.task('jshint', function() {
+  // server-side js
+  return gulp.src(serverJS).pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'));
 });
+
+// watch for changes
+gulp.task('watch', function() {
+  gulp.watch(serverJS).on('change', $.livereload.changed);
+});
+// does this work?
